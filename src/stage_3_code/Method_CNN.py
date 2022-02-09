296bdbf00b76d178afed7c14a743156c2dfbe595
chance <adclau@ucdavis.edu>
2022-02-08 22:29:54 -0800

    expand tensor dimensions, correct fc1 linear dim
    now need to correct params for all layers in cnn so that batch sizes match when passed to cross entropy loss fn

--- src/stage_3_code/Method_CNN.py
+++ src/stage_3_code/Method_CNN.py
@@ -20,7 +20,7 @@ class Method_CNN(method, nn.Module):
         self.conv1 = nn.Conv2d(1, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc1 = nn.Linear(16*4*4, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
@@ -30,6 +30,8 @@ class Method_CNN(method, nn.Module):
         # x = self.pool(self.conv1(x))
         # x = self.pool(self.conv2(x))
         x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
@@ -39,19 +41,22 @@ class Method_CNN(method, nn.Module):
         accuracy_evaluator = Evaluate_Accuracy('training evaluator', '')
 
         for epoch in range(self.max_epoch):
+            for i in range(len(X)):
+                # tensor = tensor.unsqueeze(1)  # unsqueeze(0) for rgb
+                tensor_x = np.expand_dims(np.array(X[i]), 1)
 
-            y_pred = self.forward(torch.FloatTensor(np.array(X)))
-            y_true = torch.LongTensor(np.array(y))
+                y_pred = self(torch.FloatTensor(tensor_x))
+                y_true = torch.LongTensor(np.array(y[i]))
 
-            optimizer.zero_grad()
+                optimizer.zero_grad()
 
-            train_loss = loss_function(y_pred, y_true)
-            train_loss.backward()
-            optimizer.step()
+                train_loss = loss_function(y_pred, y_true)
+                train_loss.backward()
+                optimizer.step()
 
-            if epoch%100 == 0:
-                accuracy_evaluator.data = {'true_y': y_true, 'pred_y': y_pred.max(1)[1]}
-                print('Epoch:', epoch, 'Accuracy:', accuracy_evaluator.evaluate(), 'Loss:', train_loss.item())
+                if epoch%100 == 0:
+                    accuracy_evaluator.data = {'true_y': y_true, 'pred_y': y_pred.max(1)[1]}
+                    print('Epoch:', epoch, 'Accuracy:', accuracy_evaluator.evaluate(), 'Loss:', train_loss.item())
 
         print('Finished Training')
     
