e777b4b39161ece169b0c6bbb28358e258c0836f
Jason Wu <jazwu@ucdavis.edu>
2022-02-09 17:01:42 -0800

    I don't know why this runs, but it does.
    Co-authored-by: Chance Lau <charmychancey@users.noreply.github.com>

--- src/stage_3_code/Dataset_Loader.py
+++ src/stage_3_code/Dataset_Loader.py
@@ -10,6 +10,22 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import pickle
+from torch.utils.data import Dataset, DataLoader
+import math
+
+class CustomDataset(Dataset):
+    def __init__(self, img, labels):
+        self.labels = labels
+        self.images = img
+
+    def __len__(self):
+            return len(self.labels)
+
+    def __getitem__(self, idx):
+            label = self.labels[idx]
+            image = self.images[idx]
+            sample = image, label
+            return sample
 
 class Dataset_Loader(dataset):
     data = None
@@ -19,7 +35,6 @@ class Dataset_Loader(dataset):
     transform = transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
     )
-    batch_size = 4
 
     
     def __init__(self, dName=None, dDescription=None):    # trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
@@ -30,6 +45,8 @@ class Dataset_Loader(dataset):
         super().__init__(dName, dDescription)
     
     def load(self):
+        batch_size = 60000
+
         print('loading data...')
         X_train = []
         y_train = []
@@ -47,4 +64,12 @@ class Dataset_Loader(dataset):
             y_test.append(pair['label'])
         print('training set size:', len(data['train']), 'testing set size:', len(data['test']))
 
-        return {'train': {'X': X_train, 'y': y_train}, 'test': {'X': X_test, 'y': y_test}}
+        trainset_data = CustomDataset(X_train, y_train)
+        testset_data = CustomDataset(X_test, y_test)
+        #return {'train': {'X': X_train, 'y': y_train}, 'test': {'X': X_test, 'y': y_test}}
+        # return {'train': trainset_data, 'test': testset_data}
+
+        trainloader = DataLoader(trainset_data, batch_size=batch_size, shuffle=True, num_workers=0)
+        testloader = DataLoader(testset_data, batch_size=batch_size, shuffle=False, num_workers=0)
+
+        return {'train': trainloader, 'test': testloader}
--- src/stage_3_code/Method_CNN.py
+++ src/stage_3_code/Method_CNN.py
@@ -1,5 +1,5 @@
 from src.base_class.method import method
-from src.stage_1_code.Evaluate_Accuracy import Evaluate_Accuracy
+from src.stage_3_code.Evaluate_Accuracy import Evaluate_Accuracy
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -17,12 +17,12 @@ class Method_CNN(method, nn.Module):
     def __init__(self, mName, mDescription):
         method.__init__(self, mName, mDescription)
         nn.Module.__init__(self)
-        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv1 = nn.Conv2d(1, 6, 5, dtype=torch.double)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16*4*4, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.conv2 = nn.Conv2d(6, 16, 5, dtype=torch.double)
+        self.fc1 = nn.Linear(16*4*4, 120, dtype=torch.double)
+        self.fc2 = nn.Linear(120, 84, dtype=torch.double)
+        self.fc3 = nn.Linear(84, 10, dtype=torch.double)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -35,18 +35,22 @@ class Method_CNN(method, nn.Module):
         x = self.fc3(x)
         return x
 
-    def train(self, X, y):
+    # def train(self, X, y):
+    def train(self, traindata):
         optimizer = optim.SGD(self.parameters(), lr=self.learning_rate, momentum=0.9)
         loss_function = nn.CrossEntropyLoss()
         accuracy_evaluator = Evaluate_Accuracy('training evaluator', '')
 
         for epoch in range(self.max_epoch):
-            for i in range(len(X)):
+            for i, dataset in enumerate(traindata, 0):
                 # tensor = tensor.unsqueeze(1)  # unsqueeze(0) for rgb
-                tensor_x = np.expand_dims(np.array(X[i]), 1)
+                inputs, labels = dataset
+                # tensor_x = np.expand_dims(np.array(inputs), 1)
 
-                y_pred = self(torch.FloatTensor(tensor_x))
-                y_true = torch.LongTensor(np.array(y[i]))
+                # y_pred = self(torch.FloatTensor(inputs))
+                y_pred = self(inputs.double())
+                # y_true = torch.tensor([y[i]])
+                y_true = labels
 
                 optimizer.zero_grad()
 
@@ -54,21 +58,33 @@ class Method_CNN(method, nn.Module):
                 train_loss.backward()
                 optimizer.step()
 
-                if epoch%100 == 0:
+                # if epoch%100 == 0:
+                #     accuracy_evaluator.data = {'true_y': y_true, 'pred_y': y_pred.max(1)[1]}
+                #     print('Epoch:', epoch, 'Accuracy:', accuracy_evaluator.evaluate(), 'Loss:', train_loss.item())
+
+                if i % 2000 == 1999:    # print every 2000 mini-batches
                     accuracy_evaluator.data = {'true_y': y_true, 'pred_y': y_pred.max(1)[1]}
                     print('Epoch:', epoch, 'Accuracy:', accuracy_evaluator.evaluate(), 'Loss:', train_loss.item())
-
         print('Finished Training')
     
-    def test(self, X):
-        y_pred = self.forward(torch.FloatTensor(np.array(X)))
-        return y_pred.max(1)[1]
+    def test(self, testdata):
+        dataiter = iter(testdata)
+        images, _ = dataiter.next()
+        outputs = self(images)
+        _, predicted = torch.max(outputs, 1)
+
+        return predicted
+        # y_pred = self(torch.FloatTensor(np.array(X)))
+        # return y_pred.max(1)[1]
         
 
     def run(self):
         print('method running...')
         print('--start training...')
-        self.train(self.data['train']['X'], self.data['train']['y'])
+        # self.train(self.data['train']['X'], self.data['train']['y'])
+        self.train(self.data['train'])
         print('--start testing...')
-        pred_y = self.test(self.data['test']['X'])
-        return {'pred_y': pred_y.cpu(), 'true_y': self.data['test']['y']}
+        # pred_y = self.test(self.data['test']['X'])
+        pred_y = self.test(self.data['test'])
+        # return {'pred_y': pred_y.cpu(), 'true_y': self.data['test']['y']}
+        return {'pred_y': pred_y.cpu(), 'true_y': self.data['test']}
